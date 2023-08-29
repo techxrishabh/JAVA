fd8da60b3383f9dbb37e318803b0928e5deb7c9a
rishabh22204901 <rishabh22204901@gmail.com>
2023-08-29 08:45:54 +0530

    variable done

---
+++ BASICS-OF-PROGRAMMING/Variables and Data types/Variables-and-data-types/Variables.java
@@ -0,0 +1,11 @@
+public class Variables {
+    public static void main(String args[]) {
+        int a = 10;
+        int b = 20;
+        System.out.println(a);
+        System.out.println(b);
+        String name = "Rishabh-Kumar";
+        System.out.println(name);
+
+    }
+}
