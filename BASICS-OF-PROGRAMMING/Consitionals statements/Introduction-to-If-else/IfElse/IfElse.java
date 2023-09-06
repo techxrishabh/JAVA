587cda6ef6a58ff7d644be2224842ac6ca698b9d
rishabh22204901 <rishabh22204901@gmail.com>
2023-09-06 16:08:38 +0530

    if-else done

---
+++ BASICS-OF-PROGRAMMING/Consitionals statements/Introduction-to-If-else/IfElse/IfElse.java
@@ -0,0 +1,17 @@
+package IfElse;
+
+import java.util.Scanner;
+
+public class IfElse{
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int rem = n % 2;
+        if (rem == 0){
+            System.out.println("even");
+
+        } else {
+            System.out.println("odd");
+        }
+    }
+}
